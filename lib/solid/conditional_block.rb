5ccb71ced4cc180f6c47051708b7a7331dbba84b
Jean Boussier <jean.boussier@tigerlilyapps.com>
2012-01-06 15:57:53 +0100

    Improve ConditionalBlock implementation

--- lib/solid/conditional_block.rb
+++ lib/solid/conditional_block.rb
@@ -1,25 +1,35 @@
-class Solid::ConditionalBlock < Liquid::If
-
+class Solid::ConditionalBlock < Liquid::Block
   include Solid::Element
+
+  def initialize(tag_name, variable, tokens)
+    @blocks = []
+    push_block!
+    super
+  end
 
   def render(context)
     with_context(context) do
       display(*arguments.interpolate(context)) do |condition_satisfied|
-        render_content(context, condition_satisfied)
+        block = condition_satisfied ? @blocks.first : @blocks.last
+        render_all(block, context)
       end
     end
   end
 
-  protected
-  def render_content(context, render_main_block)
-    context.stack do
-      @blocks.each do |block|
-        if render_main_block ^ block.else?
-          return render_all(block.attachment, context)
-        end
-      end
-      ''
+  def unknown_tag(tag, markup, tokens)
+    if tag == 'else'
+      push_block!
+    else
+      super
     end
+  end
+
+  private
+
+  def push_block!
+    block = []
+    @blocks.push(block)
+    @nodelist = block
   end
 
 end
